538937da723f969253ba591f771c700d23d6c4d3
Wei Lin <weisl9723@gmail.com>
2022-06-10 20:10:40 -0500

    added styling to page, color now changes on click instead of hover, and interactive buttons

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 let color = "black";
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+const blackBtn = document.getElementById('blackBtn')
+const rainbowBtn = document.getElementById('rainbowBtn')
+const eraserBtn = document.getElementById('eraserBtn')
+const resetBtn = document.getElementById('resetBtn')
+
+//blackBtn.addEventListener("click", () => {
+//   changeColor("black")
+//});
+
+blackBtn.onclick = () => changeColor("black");
+rainbowBtn.onclick = () => changeColor("rainbow");
+eraserBtn.onclick = () => changeColor("white");
+resetBtn.onclick = () => resetGrid();
+
 function createGrid(size){
     let board = document.querySelector(".board");
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -7,13 +25,26 @@ function createGrid(size){
 
     for (let i = 0; i < size * size; i++){
         let box = document.createElement("div");
-        board.appendChild(box);
+        box.addEventListener('mousedown', paintGrid);
         box.addEventListener('mouseover', paintGrid);
+        board.appendChild(box);
     }
 }
 
-function paintGrid(){
-    if (color === 'random'){
+function changeSize(userInput){
+    if (userInput >= 1 && userInput <= 64){
+        createGrid(userInput);
+        resetGrid();
+    }
+    else{
+        return;
+    }
+}
+
+function paintGrid(e){
+    if (e.type === "mouseover" && !mouseDown)
+    return;
+    if (color === 'rainbow'){
         this.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     }
     else{
@@ -21,8 +52,28 @@ function paintGrid(){
     }
 }
 
-function changeColor(choice){
-    color = choice;
+function changeColor(selection){
+    blackBtn.classList.remove('active')
+    rainbowBtn.classList.remove('active')
+    eraserBtn.classList.remove('active')
+
+    if (selection === "black"){
+        blackBtn.classList.add('active')
+    }
+    else if (selection === "rainbow"){
+        rainbowBtn.classList.add('active')
+    }
+    else if (selection === "white"){
+        eraserBtn.classList.add('active')
+    }
+
+    color = selection;
+}
+
+function resetGrid(){
+    let board = document.querySelector(".board");
+    let box = board.querySelectorAll("div");
+    box.forEach((div) => div.style.backgroundColor = "white");
 }
 
 createGrid(16);
